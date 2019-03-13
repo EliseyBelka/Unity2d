b387d33b36343995cdd9a2bbcbbd1daab228c45d
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-03-13 19:40:05 +0700

    Add files via upload
    Задание находится в процессе освоения и выполнения. Сделал ссылку чтобы не терять возможность сдать задание сегодня.

---
+++ Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject _enemy;
+    [SerializeField] Transform _spawnPoints;
+    private void OnTriggerEnter2D(Collider2D Collision)
+    {
+        if (Collision.tag=="Respawn")
+        {
+            System.Console.WriteLine("works");
+            Instantiate(_enemy, _spawnPoints.position, _spawnPoints.rotation);
+        }
+    }
+
+}
