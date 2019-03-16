42ed654d23157107a3fce450fbe2b264462e473b
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-03-16 22:24:03 +0700

    Add files via upload
    BckGrndScrip - скрипт фона
    Spr - Spear мой "булет"
    Остальные стандартные и понятные по названию

---
+++ Scrpts/Health.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField] private int health = 3;
+    public void Damage(int damage)
+    {
+        health -= damage;
+        if (health <= 0) Destroy(gameObject);
+
+    }
+}
