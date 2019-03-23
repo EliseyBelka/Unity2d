bca613373cf06a255301b871b3c3a198d9f7c600
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-03-23 19:26:03 +0700

    Add files via upload
    Задание 4 с Альфа каналом добавлено.
    Задание в процессе выполнения.

---
+++ Less5Task4/AlfaColorCub.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlfaColorCub : MonoBehaviour
+{
+    private float mySlider = 1.0f; // Положение пользовательского слайдера
+    public Color myColor;         // Градиент цвета
+    public MeshRenderer GO;      // Ссылка на рендер объекта
+    void OnGUI()
+    {
+        mySlider = LabelSlider(new Rect(10, 10, 200, 20), mySlider, 0.0f, 5.0f, "My Slider"); // Отрисовка пользовательского слайдера
+        myColor = RGBSlider(new Rect(10, 30, 200, 20), myColor);  // Отрисовка пользовательского набора слайдеров для получения градиента цвета
+        GO.material.color = myColor; // Покраска объекта
+    }
+    // Отрисовка пользовательского слайдера
+    float LabelSlider(Rect screenRect, float sliderValue, float sliderMinValue, float sliderMaxValue, string labelText)
+    {
+        // создаём прямоугольник с координатами в пространстве и заданой шириной и высотой 
+        Rect labelRect = new Rect(screenRect.x, screenRect.y, screenRect.width / 2, screenRect.height);
+        GUI.Label(labelRect, labelText);   // создаём Label на экране
+        Rect sliderRect = new Rect(screenRect.x + screenRect.width / 2, screenRect.y, screenRect.width / 2, screenRect.height); // Задаём размеры слайдера
+        sliderValue = GUI.HorizontalSlider(sliderRect, sliderValue, sliderMinValue, sliderMaxValue); // Вырисовываем слайдер и считываем его параметр
+        return sliderValue; // Возвращаем значение слайдера
+    }
+    // Отрисовка тройной слайдер группы, каждый слайдер отвечает за свой цвет
+    Color RGBSlider(Rect screenRect, Color rgb)
+    {
+        // Используя пользовательский слайдер, создаём его
+        rgb.r = LabelSlider(screenRect, rgb.r, 0.0f, 1.0f, "Red");
+        // делаем промежуток
+        screenRect.y += 20;
+        rgb.g = LabelSlider(screenRect, rgb.g, 0.0f, 1.0f, "Green");
+        screenRect.y += 20;
+        rgb.b = LabelSlider(screenRect, rgb.b, 0.0f, 1.0f, "Blue");
+        screenRect.y += 20;
+        rgb.a = LabelSlider(screenRect, rgb.a, 0.0f, 1.0f, "Alfa");
+        return rgb; // возвращаем цвет
+    }
+}
