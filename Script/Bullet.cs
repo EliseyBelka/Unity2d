9016c3d9c1b9d717df6a9efd36214ea587cbc22e
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-03-27 17:59:37 +0700

    Add files via upload

---
+++ Script/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Bullet : MonoBehaviour {
+    void Start()
+    {
+        // уничтожить объект по истечению указанного времени (секунд), если пуля никуда не попала
+        Destroy(gameObject, 5);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Hero") // чтобы пуля не реагировала на co
+        {
+            Destroy(gameObject);
+        }
+    }
+  
+}
---
+++ Script/RespawnHero.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnHero : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Hero")
+        {
+            collision.gameObject.transform.position = new Vector3(-3.49F, -3.57F, 0.0281F);
+            HealthBar.AdjustCurrentValue(-25);
+        }
+    }
+}
